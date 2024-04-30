8405a36ab06c3ab19de59ba2e3f8825f17854683
hashrock <hashrock@users.noreply.github.com>
2024-04-30 12:39:21 +0900

    use jsr

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import * as mod from "@hashrock/hr-user-csv-parser";
 import fs from "node:fs/promises";
-
 const file = await fs.readFile("users.csv", "utf-8");
-const parsed = await mod.parseUserCsv(file)
-
-console.log(parsed)
+const parsed = await mod.parseUserCsv(file);
+console.log(parsed);
