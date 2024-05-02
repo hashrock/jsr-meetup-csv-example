39a4f150deff5d8a1721c79f4c9073a6631104e5
hashrock <hashrock@users.noreply.github.com>
2024-05-03 03:44:21 +0900

    Revert "use jsr"
    This reverts commit 8405a36ab06c3ab19de59ba2e3f8825f17854683.

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import * as mod from "@hashrock/hr-user-csv-parser";
-import fs from "node:fs/promises";
-const file = await fs.readFile("users.csv", "utf-8");
-const parsed = await mod.parseUserCsv(file);
-console.log(parsed);
